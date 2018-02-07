ffc27a57803218798a17fee60f2b7269bb870693
Benjamin Angeria <benjamin@angeria.se>
2018-02-07 22:47:53 +0100

    Added function frames and more data types.

--- bitsek.hs
+++ bitsek.hs
@@ -2,30 +2,52 @@
 
 -- Authors: Benjamin Angeria, Fabian Haglund and Holger Swartling.
 
-data User = User Adress Balance
+data User = User Adress Balance deriving (Show)
 type Adress = String
 type Balance = Int
 
-data Transaction = Transaction Sender Recipient Amount
+data Transaction = Transaction Sender Recipient Amount deriving (Show)
 type Sender = String
 type Recipient = String
 type Amount = Int
 
 data Block = Block { index :: Int
-				   , transactions :: [Transaction]
-				   , proof ::
-				   , previousHash ::
-				   } deriving (Show)
+                   , transactions :: [Transaction]
+                -- , proof :: -- Type?
+                -- , previousHash :: Hash
+                   } deriving (Show)
 
-data Blockchain = Blockchain [Block]
+-- Latest block should be head of list.
+data Blockchain = EmptyBlockchain | Blockchain [Block] deriving (Show)
 
 
 ----------------
 -- BLOCKCHAIN --
 ----------------
 
-newTransaction ::  
+genesisBlock = undefined
+
+-- TODO
+newTransaction :: 
 newTransaction = undefined
 
+-- TODO
 newBlock :: 
 newBlock = undefined
+
+
+-- TODO
+{-  hashBlock block
+    Takes a block and returns the hash of it.
+-}
+hashBlock :: Block -> Hash
+hashBlock = undefined
+
+
+{-  lastBlock blockchain 
+    Takes a blockchain and returns the last block in it.
+    PRE: blockchain must be non-empty.
+-}
+lastBlock :: Blockchain -> Block
+lastBlock blockchain = case blockchain of Blockchain (x:xs) -> x
+
